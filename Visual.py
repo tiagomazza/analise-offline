7cb6b46c473c9a8772b215ae9e0bd141f4ee26f9
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-01-12 14:44:14 +0000

    upload changes

--- Visual.py
+++ Visual.py
@@ -1,6 +1,13 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import pandas as pd
+import matplotlib.pyplot as plt
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+import tempfile
+import os
+from datetime import datetime
+import win32com.client as win32
 
 # Listas globais para armazenar os valores de cada coluna
 nomeLista = []
@@ -14,13 +21,18 @@ meta4Lista = []
 class Aplicacao:
     def __init__(self, janela):
         self.janela = janela
-        self.janela.title("Carregar XLSX")
-        
+        self.janela.title("Envio de metas por email")
+
         self.dataframe = None  # Variável para armazenar o DataFrame
 
         # Criar um botão para carregar o arquivo XLSX
-        self.botao_carregar_xlsx = tk.Button(self.janela, text="Carregar XLSX", command=self.carregar_xlsx)
+        self.botao_carregar_xlsx = tk.Button(self.janela, text="Carregue os dados", command=self.carregar_xlsx)
         self.botao_carregar_xlsx.pack(pady=20)
+
+        # Criar um segundo botão para carregar o arquivo CSV
+        self.botao_carregar_csv = tk.Button(self.janela, text="Carregar CSV", command=self.carregar_csv)
+        self.botao_carregar_csv.pack(pady=20)
+        self.botao_carregar_csv.pack_forget()  # Ocultar o segundo botão inicialmente
 
     def carregar_xlsx(self):
         # Abrir a caixa de diálogo para seleção do arquivo XLSX
@@ -36,19 +48,42 @@ class Aplicacao:
                 nomeLista = self.dataframe['Nome'].astype(str).tolist()
                 emailVendedorLista = self.dataframe['Email'].astype(str).tolist()
                 codigoVendedorLista = self.dataframe['Código'].astype(str).tolist()
-                meta1Lista = self.dataframe['Meta 1'].astype(str).tolist()
-                meta2Lista = self.dataframe['Meta 2'].astype(str).tolist()
-                meta3Lista = self.dataframe['Meta 3'].astype(str).tolist()
-                meta4Lista = self.dataframe['Meta 4'].astype(str).tolist()
+                codigoVendedorLista = self.dataframe['Código'].apply(lambda x: f'{int(x):02}').tolist()
+                meta1Lista = self.dataframe['Meta 1'].astype(int).tolist()
+                meta2Lista = self.dataframe['Meta 2'].astype(int).tolist()
+                meta3Lista = self.dataframe['Meta 3'].astype(int).tolist()
+                meta4Lista = self.dataframe['Meta 4'].astype(int).tolist()
 
                 # Exibir mensagem de sucesso
-                mensagem_sucesso = "XLSX carregado com sucesso!"
-                tk.messagebox.showinfo("Sucesso", mensagem_sucesso)
+                mensagem_sucesso = "XLSX carregado com sucesso! Feche o programa que enviaremos os emails"
+                messagebox.showinfo("Sucesso", mensagem_sucesso)
+
+                # Mostrar o segundo botão após o carregamento do XLSX
+                self.botao_carregar_csv.pack()
+
             except Exception as e:
                 # Exibir mensagem de erro
-                tk.messagebox.showerror("Erro", f"Erro ao carregar o arquivo XLSX: {str(e)}")
+                messagebox.showerror("Erro", f"Erro ao carregar o arquivo XLSX: {str(e)}")
 
-# Criar a janela principal
+    def carregar_csv(self):
+        # Abrir a caixa de diálogo para seleção do arquivo CSV
+        caminho_csv = filedialog.askopenfilename(filetypes=[("Arquivos CSV", "*.csv")])
+
+        if caminho_csv:
+            try:
+                # Ler o arquivo CSV
+                dataframe_csv = pd.read_csv(caminho_csv, encoding='latin-1', decimal=',', header=0, skiprows=1)
+
+                # Realizar operações desejadas com o dataframe_csv, se necessário
+                # ...
+
+                # Exibir mensagem de sucesso
+                mensagem_sucesso = "CSV carregado com sucesso!"
+                messagebox.showinfo("Sucesso", mensagem_sucesso)
+
+            except Exception as e:
+                # Exibir mensagem de erro
+                messagebox.showerror("Erro", f"Erro ao carregar o arquivo CSV: {str(e)}")
 janela_principal = tk.Tk()
 
 # Criar uma instância da classe Aplicacao
@@ -56,13 +91,3 @@ app = Aplicacao(janela_principal)
 
 # Iniciar o loop principal
 janela_principal.mainloop()
-
-# Imprimir o conteúdo das listas no final do código
-print("Conteúdo das Listas:")
-print("nomeLista:", nomeLista)
-print("emailVendedorLista:", emailVendedorLista)
-print("codigoVendedorLista:", codigoVendedorLista)
-print("meta1Lista:", meta1Lista)
-print("meta2Lista:", meta2Lista)
-print("meta3Lista:", meta3Lista)
-print("meta4Lista:", meta4Lista)
--- relatorioComEnvioVisual.py
+++ relatorioComEnvioVisual.py
@@ -1,28 +1,118 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
 import pandas as pd
 import matplotlib.pyplot as plt
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 import tempfile
-import os 
+import os
 from datetime import datetime
 import win32com.client as win32
+
+# Listas globais para armazenar os valores de cada coluna
+nomeLista = []
+emailVendedorLista = []
+codigoVendedorLista = []
+meta1Lista = []
+meta2Lista = []
+meta3Lista = []
+meta4Lista = []
+
+class Aplicacao:
+    def __init__(self, janela):
+        self.janela = janela
+        self.janela.title("METAS")
+
+        self.dataframe = None  # Variável para armazenar o DataFrame
+
+        # Criar um botão para carregar o arquivo XLSX
+        self.botao_carregar_xlsx = tk.Button(self.janela, text="Carregue os dados basicos da análise", command=self.carregar_xlsx)
+        self.botao_carregar_xlsx.pack(pady=20)
+
+        # Criar um segundo botão para carregar o arquivo CSV
+        self.botao_carregar_csv = tk.Button(self.janela, text="Carregar a planilha com os dados a serem analisados", command=self.carregar_csv)
+        self.botao_carregar_csv.pack(pady=20)
+        self.botao_carregar_csv.pack_forget()  # Ocultar o segundo botão inicialmente
+
+    def carregar_xlsx(self):
+        # Abrir a caixa de diálogo para seleção do arquivo XLSX
+        caminho_arquivo = filedialog.askopenfilename(filetypes=[("Arquivos Excel", "*.xlsx")])
+
+        if caminho_arquivo:
+            try:
+                # Ler o arquivo XLSX e criar um DataFrame
+                self.dataframe = pd.read_excel(caminho_arquivo)
+
+                # Preencher as listas globais com os valores de cada coluna
+                global nomeLista, emailVendedorLista, codigoVendedorLista, meta1Lista, meta2Lista, meta3Lista, meta4Lista
+                nomeLista = self.dataframe['Nome'].astype(str).tolist()
+                emailVendedorLista = self.dataframe['Email'].astype(str).tolist()
+                codigoVendedorLista = self.dataframe['Código'].astype(str).tolist()
+                codigoVendedorLista = self.dataframe['Código'].apply(lambda x: f'{int(x):02}').tolist()
+                meta1Lista = self.dataframe['Meta 1'].astype(int).tolist()
+                meta2Lista = self.dataframe['Meta 2'].astype(int).tolist()
+                meta3Lista = self.dataframe['Meta 3'].astype(int).tolist()
+                meta4Lista = self.dataframe['Meta 4'].astype(int).tolist()
+
+                # Exibir mensagem de sucesso
+                mensagem_sucesso = "XLSX carregado com sucesso! Feche o programa que enviaremos os emails"
+                messagebox.showinfo("Sucesso", mensagem_sucesso)
+
+                # Mostrar o segundo botão após o carregamento do XLSX
+                self.botao_carregar_csv.pack()
+
+            except Exception as e:
+                # Exibir mensagem de erro
+                messagebox.showerror("Erro", f"Erro ao carregar o arquivo XLSX: {str(e)}")
+
+    def carregar_csv(self):
+        global caminho_csv
+        # Abrir a caixa de diálogo para seleção do arquivo CSV
+        caminho_csv = filedialog.askopenfilename(filetypes=[("Arquivos CSV", "*.csv")])
+
+        if caminho_csv:
+            try:
+                # Ler o arquivo CSV
+                dataframe_csv = pd.read_csv(caminho_csv, encoding='latin-1', decimal=',', header=0, skiprows=1)
+
+                # Realizar operações desejadas com o dataframe_csv, se necessário
+                # ...
+
+                # Exibir mensagem de sucesso
+                mensagem_sucesso = "CSV carregado com sucesso!"
+                messagebox.showinfo("Sucesso", mensagem_sucesso)
+
+            except Exception as e:
+                # Exibir mensagem de erro
+                messagebox.showerror("Erro", f"Erro ao carregar o arquivo CSV: {str(e)}")
+janela_principal = tk.Tk()
+
+# Criar uma instância da classe Aplicacao
+app = Aplicacao(janela_principal)
+
+# Iniciar o loop principal
+janela_principal.mainloop()
+
+# Imprimir o conteúdo das listas no final do código
+print("Conteúdo das Listas:")
+print("nomeLista:", nomeLista)
+print("emailVendedorLista:", emailVendedorLista)
+print("codigoVendedorLista:", codigoVendedorLista)
+print("meta1Lista:", meta1Lista)
+print("meta2Lista:", meta2Lista)
+print("meta3Lista:", meta3Lista)
+print("meta4Lista:", meta4Lista)
+
+
+
 
 data_e_horario_atual = datetime.now()
 data_e_horario_formatados = data_e_horario_atual.strftime("%d-%m-%Y %H:%M")
 
 
-nomeLista =["Diogo","Orlando","Hugo","Vitor","Fernando",]
-emailVendedorLista =["tiagomazza@gmail.com","tiagomazza@gmail.com","tiagomazza@gmail.com","tiagomazza@gmail.com","tiagomazza@gmail.com"]
-#emailVendedor =["diogo.bento@aborgesdoamaral.pt","orlando.cardoso@aborgesdoamaral.pt","hugo.tavares@aborgesdoamaral.pt","vitor.teixeira@aborgesdoamaral.pt","fernando.ladeiro@aborgesdoamaral.pt",]
-codigoVendedorLista =["05","30","12","06","08"]
-meta1Lista =[1000,1000,1000,1000,1000]
-meta2Lista =[2000,2000,2000,2000,2000]
-meta3Lista =[3000,3000,3000,3000,3000]
-meta4Lista =[4000,4000,4000,4000,4000]
-
 for salesman, emailVendedor, codigoVendedor, meta1, meta2, meta3, meta4 in zip (nomeLista, emailVendedorLista, codigoVendedorLista, meta1Lista, meta2Lista, meta3Lista, meta4Lista):
-    caminho_arquivo_csv = 'analise.csv'
-    dataframe = pd.read_csv(caminho_arquivo_csv, encoding='latin-1', decimal=',', header=0, skiprows=1)
+    #caminho_arquivo_csv = 'analise.csv'
+    dataframe = pd.read_csv(caminho_csv, encoding='latin-1', decimal=',', header=0, skiprows=1)
     dataframe['Cliente'] = dataframe['Cliente'].str.replace(r'\D', '', regex=True)
     dataframe['Vd'] = dataframe['Vd'].str.replace(r'\D', '', regex=True)
     dataframe['Ano'] = pd.to_datetime(dataframe['Ano'], format='%Y')
@@ -155,7 +245,7 @@ for salesman, emailVendedor, codigoVendedor, meta1, meta2, meta3, meta4 in zip (
         
         attachment_path = localDoArquivo
         email.Attachments.Add(attachment_path)
-        #email.Send()
+        email.Send()
         print('E-mail enviado com sucesso!')
     except Exception as e:
         print(f'Erro: {e}')
