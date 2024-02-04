f86c5fe8fd9cf64aad7839c5abb405a797ca8cce
Tiago Mazza <tiagomazza@gmail.com>
2024-02-04 14:04:59 +0000

    update estudos

--- Estudos/janelaTkinter.py
+++ Estudos/janelaTkinter.py
@@ -2,12 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 
-# Suponha que você já tenha um DataFrame chamado 'df'
-# Aqui, estamos apenas criando um DataFrame de exemplo
 data = {'Column1': [1, 2, 3], 'Column2': [4, 5, 6], 'Column3': [7, 8, 9]}
 df = pd.DataFrame(data)
 
-# Variável para armazenar a coluna selecionada
 coluna_selecionada = None
 
 def on_select(event):
@@ -16,23 +13,41 @@ def on_select(event):
     coluna_selecionada = selected_column
     print("Coluna selecionada:", coluna_selecionada)
 
-# Criar a janela principal
-root = tk.Tk()
-root.title("Caixa Suspensa de Colunas")
+janela = tk.Tk()
+janela.title("Titulo da janela")
+janela.iconbitmap('icone.ico')
+janela.configure(bg="#add8e6")
+janela.geometry("200x400")
 
-# Obter as colunas do DataFrame
+
 columns = df.columns.tolist()
 
 # Criar a caixa suspensa (Combobox)
-column_combobox = ttk.Combobox(root, values=columns)
-column_combobox.set("Selecione uma coluna")  # Texto padrão exibido na caixa suspensa
+column_combobox = ttk.Combobox(janela, values=columns)
+column_combobox.set("Selecione uma coluna")  
 column_combobox.bind("<<ComboboxSelected>>", on_select)
-
-# Exibir a caixa suspensa
 column_combobox.pack(pady=10)
 
-# Iniciar o loop principal do Tkinter
-root.mainloop()
+# Criar a checkbox
+check_var = tk.BooleanVar()
+checkbox = tk.Checkbutton(janela, text="Texto do checkbox", variable=check_var)
+checkbox.pack(pady=10)
 
-# Agora você pode usar a variável 'coluna_selecionada' para acessar a coluna escolhida em outros lugares do seu código.
+# Criar botão
+def exibir_mensagem():
+    print("Pressionado")
+
+botao = tk.Button(janela, text="Pressione-me", command=exibir_mensagem)
+botao.pack(pady=10)
+
+spin_valor = tk.Spinbox(janela, from_=0, to=100, increment=1)
+spin_valor.pack(pady=10)
+
+        
+
+
+janela.mainloop()
+
 print (coluna_selecionada)
+print (check_var)
+print(f"Valor do Spinbox: {valor}")
